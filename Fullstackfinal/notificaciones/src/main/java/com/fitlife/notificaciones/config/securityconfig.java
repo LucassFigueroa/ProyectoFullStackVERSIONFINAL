6d30a313dae3dc4c7ac2ecf9d16a094adcf30ba4
Santiago <sa.serrano@duocuc.cl>
2025-06-23 02:39:20 -0400

    Swagger config (Pendiente)
    horario,inventario,membresia,notificaciones,pago

---
+++ Fullstackfinal/notificaciones/src/main/java/com/fitlife/notificaciones/config/securityconfig.java
@@ -0,0 +1,18 @@
+package com.fitlife.notificaciones.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class securityconfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable()) // Desactiva CSRF, no lo necesitas en REST puro
+            .authorizeHttpRequests(auth -> auth.anyRequest().permitAll()); //  Permite todo acceso sin login
+        return http.build();
+    }
+}
